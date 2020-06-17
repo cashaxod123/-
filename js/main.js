edacea063a1f78579db7e69a8677bc3c3f7a7e7e
sachaxod <cashaxod@yandex.ru>
2020-06-17 22:52:40 +0300

    Урок 2 2 часа

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function() {
-    $('.faity-tail__slider').slick({
+    $('.faity-tail__slider, .our-trip__slider').slick({
         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-prev.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
         autoplay: true,
